1b1005e8c9c47fb60b65a69735a11f92e9fde656
KrishnaKoushikElluru <koushikakhil895@gmail.com>
2026-01-07 16:28:43 +0530

    first kk's commit

---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern{
+    public static void main(String[] args){
+        n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i%2==0){
+                    system.out.println("*");
+                }
+
+            }
+        }
+    }
+}
